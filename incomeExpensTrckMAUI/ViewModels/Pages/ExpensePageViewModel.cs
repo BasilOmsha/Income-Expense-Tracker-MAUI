d6dd9246cf36b908ae84b322eec643a24eb301ad
Basel <107993017+BasilOmsha@users.noreply.github.com>
2024-04-11 20:45:15 +0300

    The command that navigates to the 'Add Expense' page uses now Mvvm communty toolkit instead of MvvmHelper

--- incomeExpensTrckMAUI/ViewModels/Pages/ExpensePageViewModel.cs
+++ incomeExpensTrckMAUI/ViewModels/Pages/ExpensePageViewModel.cs
@@ -13,18 +13,19 @@ namespace incomeExpensTrckMAUI.ViewModels.Pages
     {
         private readonly ExpenseService expenseService; // this is a field to hold the service. Meaning it is a private variable that can be accessed by all methods in this class.
         public ObservableCollection<Expense> Expenses { get; private set; } = new(); // this is a property that can be accessed by the view. It is a collection of expenses.
-        public AsyncCommand AddCommand { get; }
+        //public AsyncCommand AddCommand { get; }
 
         // Implements dependency injection to inject the ExpenseService into the constructor.
         public ExpensePageViewModel(ExpenseService expenseService)
         {
             Title = "Exp. Page";
             //GenerateDummyExpenses();
-            AddCommand = new AsyncCommand(AddExpense);
+            //AddCommand = new AsyncCommand(AddExpense);
             this.expenseService = expenseService;
         }
 
-        async Task AddExpense()
+        [RelayCommand]
+        async Task NavToAddExpense()
         {
             /*var name = await App.Current.MainPage.DisplayPromptAsync("Name", "Name of coffee");
             var roaster = await App.Current.MainPage.DisplayPromptAsync("Roaster", "Roaster of coffee");
@@ -64,86 +65,5 @@ namespace incomeExpensTrckMAUI.ViewModels.Pages
                 IsRefreshing = false;
             }
         }
-
-        //private void GenerateDummyExpenses()
-        //{
-        //    var expense = new Expense
-        //    {
-        //        Date = DateTimeOffset.Now,
-        //        Amount = 100,
-        //        //Category = new List<ExpenseCategory> { new ExpenseCategory { Name = "Groceries" } },
-        //        //Account = new List<Account> { new Account { Name = "Checking" } },
-        //        Note = "Paid with cash",
-        //        Description = "Weekly Groceries"
-        //    };
-
-        //    expense.Category.Add(new ExpenseCategory { Name = "Groceries" });
-        //    expense.Account.Add(new Account { Name = "Cash" });
-
-        //    Expenses.Add(expense);
-
-
-
-        //    var expense2 = new Expense
-        //    {
-        //        Date = DateTimeOffset.Now,
-        //        Amount = 102,
-        //        //Category = new List<ExpenseCategory> { new ExpenseCategory { Name = "Groceries" } },
-        //        //Account = new List<Account> { new Account { Name = "Checking" } },
-        //        Note = "Paid with cash2",
-        //        Description = "Weekly Groceries2"
-        //    };
-
-        //    expense2.Category.Add(new ExpenseCategory { Name = "Groceries2" });
-        //    expense2.Account.Add(new Account { Name = "Cash2" });
-
-        //    Expenses.Add(expense2);
-
-
-        //    var expense3 = new Expense
-        //    {
-        //        Date = DateTimeOffset.Now.AddDays(-1),
-        //        Amount = 103,
-        //        //Category = new List<ExpenseCategory> { new ExpenseCategory { Name = "Groceries" } },
-        //        //Account = new List<Account> { new Account { Name = "Checking" } },
-        //        Note = "Paid with cash3",
-        //        Description = "Weekly Groceries3"
-        //    };
-
-        //    expense3.Category.Add(new ExpenseCategory { Name = "Groceries3" });
-        //    expense3.Account.Add(new Account { Name = "Cash3" });
-
-        //    Expenses.Add(expense3);
-
-        //    var expense4 = new Expense
-        //    {
-        //        Date = DateTimeOffset.Now.AddDays(-1),
-        //        Amount = 103,
-        //        //Category = new List<ExpenseCategory> { new ExpenseCategory { Name = "Groceries" } },
-        //        //Account = new List<Account> { new Account { Name = "Checking" } },
-        //        Note = "Paid with cash4",
-        //        Description = "Weekly Groceries4"
-        //    };
-
-        //    expense4.Category.Add(new ExpenseCategory { Name = "Groceries4" });
-        //    expense4.Account.Add(new Account { Name = "Cash4" });
-
-        //    Expenses.Add(expense4);
-
-        //    var expense5 = new Expense
-        //    {
-        //        Date = DateTimeOffset.Now.AddDays(-1),
-        //        Amount = 103,
-        //        //Category = new List<ExpenseCategory> { new ExpenseCategory { Name = "Groceries" } },
-        //        //Account = new List<Account> { new Account { Name = "Checking" } },
-        //        Note = "Paid with cash35",
-        //        Description = "Weekly Groceries5"
-        //    };
-
-        //    expense5.Category.Add(new ExpenseCategory { Name = "Groceries5" });
-        //    expense5.Account.Add(new Account { Name = "Cash5" });
-
-        //    Expenses.Add(expense5);
-        //}
     }
 }
