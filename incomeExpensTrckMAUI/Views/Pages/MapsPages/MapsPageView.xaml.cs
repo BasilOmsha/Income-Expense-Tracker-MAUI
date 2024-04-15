734369b4f087156823c9f290e84b343b0674c435
Basel <107993017+BasilOmsha@users.noreply.github.com>
2024-04-15 22:48:02 +0300

    Maps permissions for Android set.

--- incomeExpensTrckMAUI/Views/Pages/MapsPages/MapsPageView.xaml.cs
+++ incomeExpensTrckMAUI/Views/Pages/MapsPages/MapsPageView.xaml.cs
@@ -2,12 +2,50 @@ namespace incomeExpensTrckMAUI.Views.Pages.MapsPages;
 
 public partial class MapsPageView : ContentPage
 {
-	public MapsPageView()
+    private CancellationTokenSource _cancelTokenSource;
+    private bool _isCheckingLocation;
+    public MapsPageView()
 	{
 		InitializeComponent();
 
         var mapControl = new Mapsui.UI.Maui.MapControl();
         mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
         Content = mapControl;
+        _ = GetCurrentLocation();
+    }
+
+    public async Task GetCurrentLocation()
+    {
+        try
+        {
+            _isCheckingLocation = true;
+
+            GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+            _cancelTokenSource = new CancellationTokenSource();
+
+            Location location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
+
+            if (location != null)
+                Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+        }
+        // Catch one of the following exceptions:
+        //   FeatureNotSupportedException
+        //   FeatureNotEnabledException
+        //   PermissionException
+        catch (Exception ex)
+        {
+            // Unable to get location
+        }
+        finally
+        {
+            _isCheckingLocation = false;
+        }
+    }
+
+    public void CancelRequest()
+    {
+        if (_isCheckingLocation && _cancelTokenSource != null && _cancelTokenSource.IsCancellationRequested == false)
+            _cancelTokenSource.Cancel();
     }
 }
