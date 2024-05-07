2402c7a090aaedb84ae1edc8ef4bef9635198929
Basel <107993017+BasilOmsha@users.noreply.github.com>
2024-05-07 16:10:58 +0300

    Title upadated

--- incomeExpensTrckMAUI/ViewModels/Pages/MapsPageViewModel.cs
+++ incomeExpensTrckMAUI/ViewModels/Pages/MapsPageViewModel.cs
@@ -29,7 +29,7 @@ namespace incomeExpensTrckMAUI.ViewModels.Pages
 
         public MapsPageViewModel(IGeolocation geolocation, ExpenseService expenseService)
         {
-            Title = "Expense Map Maui";
+            Title = "Exp. Locations Maui";
             this.geolocation = geolocation;
             this.expenseService = expenseService;
             MapViewElement = new MapView();
