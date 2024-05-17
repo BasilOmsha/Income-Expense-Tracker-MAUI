61a8aba7a241d940de71161b6fa2babd17d21cba
Basel <107993017+BasilOmsha@users.noreply.github.com>
2024-05-17 14:12:35 +0300

    Home page activated. Expenses total function created. Main page UI modified.

--- incomeExpensTrckMAUI/ViewModels/Pages/MainPageViewModel.cs
+++ incomeExpensTrckMAUI/ViewModels/Pages/MainPageViewModel.cs
@@ -1,50 +1,87 @@
-﻿using incomeExpensTrckMAUI.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using incomeExpensTrckMAUI.Models;
+using incomeExpensTrckMAUI.Services;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace incomeExpensTrckMAUI.ViewModels.Pages
 {
     public partial class MainPageViewModel : BaseViewModel
     {
+        private readonly ExpenseService expenseService;
         public ObservableCollection<Expense> Expenses { get; private set; } = new();
         public ObservableCollection<Income> Incomes { get; private set; } = new();
 
-        public MainPageViewModel()
+        public MainPageViewModel(ExpenseService expenseService)
         {
             Title = "Main Page Maui";
-            GenerateDummyExpenses();
-            GenerateDummyIncomes();
+            this.expenseService = expenseService;
+            //GenerateDummyExpenses();
+            //GenerateDummyIncomes();
+            ExpensesTotal();
         }
 
-        private void GenerateDummyExpenses()
+        [ObservableProperty]
+        bool isRefreshing;
+
+        [ObservableProperty]
+        double totalExpenses;
+
+        [RelayCommand]
+        void ExpensesTotal()
         {
-            var expense = new Expense
+            if (IsLoading) return;
+            try
             {
-                //Date = DateTimeOffset.Now.ToString(),
-                Amount = 100,
-                Category = "Groceries",
-                Account = "Checking",
-                Location = "Local Supermarket",
-                Note = "Weekly groceries",
-                Description = "Food and supplies"
-            };
+                IsLoading = true;
+                if (Expenses.Any()) Expenses.Clear();
+                TotalExpenses = 0;
 
-            Expenses.Add(expense);
+                var expenses = expenseService.GetExpenses();
 
+                foreach (var expense in expenses) Expenses.Add(expense);
+
+                foreach (var exp in Expenses) TotalExpenses += exp.Amount; 
+                Debug.WriteLine($"Total Expenses: {TotalExpenses}");
+            }
+            finally
+            {
+                IsLoading = false;
+                IsRefreshing = false;
+            }
         }
 
-        private void GenerateDummyIncomes()
-        {
-            var income = new Income
-            {
-                Date = DateTimeOffset.Now,
-                Amount = 100,
-                Category = "Salary",
-                Account = "Card",
-                Note = "Monthly salary",
-                Description = "Salary"
-            };
+        //private void GenerateDummyExpenses()
+        //{
+        //    var expense = new Expense
+        //    {
+        //        //Date = DateTimeOffset.Now.ToString(),
+        //        Amount = 100,
+        //        Category = "Groceries",
+        //        Account = "Checking",
+        //        Location = "Local Supermarket",
+        //        Note = "Weekly groceries",
+        //        Description = "Food and supplies"
+        //    };
 
-            Incomes.Add(income);
-        }
+        //    Expenses.Add(expense);
+
+        //}
+
+        //private void GenerateDummyIncomes()
+        //{
+        //    var income = new Income
+        //    {
+        //        Date = DateTimeOffset.Now,
+        //        Amount = 100,
+        //        Category = "Salary",
+        //        Account = "Card",
+        //        Note = "Monthly salary",
+        //        Description = "Salary"
+        //    };
+
+        //    Incomes.Add(income);
+        //}
     }
 }
